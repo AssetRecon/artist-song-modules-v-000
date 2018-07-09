7dacff651408f29590271d1ad978d611f59f4df2
Edsel Caprice <caprice@mail.usf.edu>
2018-07-09 00:58:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
   attr_accessor :name
@@ -15,7 +16,7 @@ class Artist
   #end
 
   def initialize
-    @@all << self
+    super
     @songs = []
   end
 
